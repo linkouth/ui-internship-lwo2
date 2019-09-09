c5bb3505568a4320e11669c290ed88d48bf4bbaa
Timur <dkatimullin@gmail.com>
2019-09-10 01:23:40 +0400

    Implements reverseString function in 6_reverse-string.

---
+++ tasks/6_reverse-string/reverse-string.js
@@ -0,0 +1,9 @@
+/**
+ * reverseString - reverses the provided string
+ *
+ * @param {String} str
+ * @return {String}
+ */
+export function reverseString(str) {
+  return str.split('').reverse().join('');
+}
