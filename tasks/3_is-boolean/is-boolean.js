d314474ef0900505dbae7a5fe084ee2428697a6e
Timur <dkatimullin@gmail.com>
2019-09-10 00:13:10 +0400

    Implements isBoolean function in 3_is-boolean.

---
+++ tasks/3_is-boolean/is-boolean.js
@@ -0,0 +1,9 @@
+/**
+ * convertToF - convert from Celsius to Fahrenheit
+ *
+ * @param {*} bool
+ * @return {Boolean}
+ */
+export function isBoolean(bool) {
+  return typeof(bool) === 'boolean';
+}
