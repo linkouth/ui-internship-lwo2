3902f60859078e09bbd01b2e490a7e67d4d3c0ac
Timur <dkatimullin@gmail.com>
2019-09-10 16:48:01 +0400

    Implements titleCase function in 12_title-case.

---
+++ tasks/12_title-case/title-case.js
@@ -0,0 +1,12 @@
+/**
+ * titleCase - Return the provided string
+ * with the first letter of each word capitalized
+ *
+ * @param {String} str
+ * @return {String}
+ */
+export function titleCase(str) {
+  let words = str.toLowerCase().split(' ');
+  words = words.map((word) => word.charAt(0).toUpperCase() + word.substr(1));
+  return words.join(' ');
+}
