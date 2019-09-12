2d4ef99fd84648f275429f5ca37b851d90fdf758
Timur <dkatimullin@gmail.com>
2019-09-12 10:46:08 +0400

    Implements maxTripletSum function in 34_max-triplet-sum.

---
+++ tasks/34_max-triplet-sum/max-triplet-sum.js
@@ -0,0 +1,28 @@
+/**
+ * maxTripletSum - find maximum triplet sum
+ * in the array without duplicates
+ *
+ * @param {Array<Number>} numbers
+ * @return {Number}
+ */
+export function maxTripletSum(numbers) {
+  return maxNSum(3)(numbers);
+}
+
+/**
+ * maxNSum - find maximum n sum
+ * in the array without duplicates
+ *
+ * @param {Number} n
+ * @return {Function}
+ */
+export function maxNSum(n) {
+  return function(numbers) {
+    let sortedNumbers = numbers
+        .filter((value, index) => index === numbers.indexOf(value))
+        .sort((a, b) => a - b);
+    return sortedNumbers
+        .slice(sortedNumbers.length - n)
+        .reduce((acc, val) => acc + val, 0);
+  };
+}
