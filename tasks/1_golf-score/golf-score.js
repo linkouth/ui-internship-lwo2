5d7b8c7f0e970280f90a090013f0c8f8946e013d
Timur <dkatimullin@gmail.com>
2019-09-13 10:38:53 +0400

    Refactores golfScore function.

--- tasks/1_golf-score/golf-score.js
+++ tasks/1_golf-score/golf-score.js
@@ -20,25 +20,43 @@ export const GOLF_SCORES = {
  * @return {String}
  */
 export function golfScore(par, strokes) {
-  if (strokes === 1) {
-    return GOLF_SCORES.HOLE_IN_ONE;
+  switch (strokes) {
+    case 1:
+      return GOLF_SCORES.HOLE_IN_ONE;
+    case par - 1:
+      return GOLF_SCORES.BIRDIE;
+    case par:
+      return GOLF_SCORES.PAR;
+    case par + 1:
+      return GOLF_SCORES.BOGEY;
+    case par + 2:
+      return GOLF_SCORES.DOUBLE_BOGEY;
   }
   if (strokes <= par - 2) {
     return GOLF_SCORES.EAGLE;
   }
-  if (strokes === par - 1) {
-    return GOLF_SCORES.BIRDIE;
-  }
-  if (strokes === par) {
-    return GOLF_SCORES.PAR;
-  }
-  if (strokes === par + 1) {
-    return GOLF_SCORES.BOGEY;
-  }
-  if (strokes === par + 2) {
-    return GOLF_SCORES.DOUBLE_BOGEY;
-  }
   if (strokes >= par + 3) {
     return GOLF_SCORES.GO_HOME;
   }
+  // if (strokes === 1) {
+  //   return GOLF_SCORES.HOLE_IN_ONE;
+  // }
+  // if (strokes <= par - 2) {
+  //   return GOLF_SCORES.EAGLE;
+  // }
+  // if (strokes === par - 1) {
+  //   return GOLF_SCORES.BIRDIE;
+  // }
+  // if (strokes === par) {
+  //   return GOLF_SCORES.PAR;
+  // }
+  // if (strokes === par + 1) {
+  //   return GOLF_SCORES.BOGEY;
+  // }
+  // if (strokes === par + 2) {
+  //   return GOLF_SCORES.DOUBLE_BOGEY;
+  // }
+  // if (strokes >= par + 3) {
+  //   return GOLF_SCORES.GO_HOME;
+  // }
 }
