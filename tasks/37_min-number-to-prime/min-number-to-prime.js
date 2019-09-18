e66676499e2fbcf6597f42b4885b30c39ad4b9ac
Timur <dkatimullin@gmail.com>
2019-09-18 22:36:14 +0400

    Refactores

--- tasks/37_min-number-to-prime/min-number-to-prime.js
+++ tasks/37_min-number-to-prime/min-number-to-prime.js
@@ -8,15 +8,24 @@
  */
 export function minNumberToPrime(arr) {
   const sum = arr.reduce((acc, val) => acc + val, 0);
-  const isPrime = (num) => {
-    for (let i = 2; i <= Math.round(Math.sqrt(num)); i++) {
-      if (num % i === 0) return false;
-    }
-    return true;
-  };
   let ans = sum;
   while (!isPrime(ans)) {
     ans++;
   }
   return ans - sum;
 }
+
+/**
+ * isPrime - checks is number prime or not
+ * @example
+ * // return true
+ * isPrime(17);
+ * @param {Number} num
+ * @return {Boolean}
+ */
+const isPrime = (num) => {
+  for (let i = 2; i <= Math.round(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
