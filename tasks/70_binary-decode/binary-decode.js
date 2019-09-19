a64d0839eba053dd6bc30f41a7592d0a0a1f856a
Timur <dkatimullin@gmail.com>
2019-09-19 19:00:57 +0400

    Implements binaryDecode function in 70_binary-decode.

---
+++ tasks/70_binary-decode/binary-decode.js
@@ -0,0 +1,16 @@
+/**
+ * binaryDecode - returns an English translated sentence of
+ * the passed binary string
+ * @example
+ * // return 'fire'
+ * binaryDecode('01100110 01101001 01110010 01100101');
+ * @param {String} str
+ * @return {String}
+ */
+export function binaryDecode(str) {
+  return str === undefined ? '' :
+  str
+      .split(' ')
+      .map((word) => String.fromCharCode(Number.parseInt(word, 2)))
+      .join('');
+}
