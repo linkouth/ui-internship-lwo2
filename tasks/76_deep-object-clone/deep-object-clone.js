6829b6fc91acbdfc1b40df9d2fdfa5fa18392ec0
Timur <dkatimullin@gmail.com>
2019-09-20 16:11:51 +0400

    Implements deepClone function in 76_deep-object-clone.

---
+++ tasks/76_deep-object-clone/deep-object-clone.js
@@ -0,0 +1,21 @@
+/**
+ * deepClone - creates a recursive function to copy an argument object
+ * @example
+ * // return copy == {a: 1, b:{c:10}}
+ * var original = {a: 1, b:{c:10}};
+ * var copy = deepClone(original);
+ * @param {Object} obj
+ * @return {Object}
+ */
+export function deepClone(obj) {
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+  let result = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key]);
+    }
+  }
+  return result;
+}
