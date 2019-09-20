97273ca6a38a16ace95c8dc6fe0a5a6e83bc26e5
Timur <dkatimullin@gmail.com>
2019-09-20 19:52:26 +0400

    Implements bouncingBall function in 82_bouncing-balls.

---
+++ tasks/82_bouncing-balls/bouncing-balls.js
@@ -0,0 +1,15 @@
+/**
+ * bouncingBall - returns how many times will the mother see the ball
+ * either falling or bouncing in front of the window
+ * @example
+ * // return 15
+ * bouncingBall(30.0, 0.66, 1.5);
+ * @param {Number} h - the height of the floor
+ * @param {Number} bounce - the ball bounces for (bounce * h) metres
+ * @param {Number} window - the height of the window
+ * @return {Number}
+ */
+export function bouncingBall(h, bounce, window) {
+  let times = Math.floor(Math.log(window / h) / Math.log(bounce));
+  return times * 2 + 1;
+}
