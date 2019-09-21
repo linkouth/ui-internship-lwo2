10caf9a515ce63d49001c485da2825ba16f8c831
Timur <dkatimullin@gmail.com>
2019-09-21 19:57:41 +0400

    Implements Person class in 91_make-a-person.

---
+++ tasks/91_make-a-person/make-a-person.js
@@ -0,0 +1,65 @@
+/**
+ * class Person
+ */
+export class Person {
+  /**
+   * Constructor takes full name
+   * @param {String} fullName
+   */
+  constructor(fullName) {
+    this.setFullName(fullName);
+  }
+
+  /**
+   * getFirstName - returns first name
+   * @return {String}
+   */
+  getFirstName() {
+    return this.firstName;
+  }
+
+  /**
+   * getLastName - returns last name
+   * @return {String}
+   */
+  getLastName() {
+    return this.lastName;
+  }
+
+  /**
+   * getFullName - returns full name
+   * @return {String}
+   */
+  getFullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  /**
+   * setFirstName - set new first name
+   * @param {String} first
+   * @return {void}
+   */
+  setFirstName(first) {
+    this.firstName = first;
+  }
+
+  /**
+   * setLastName - set new last name
+   * @param {String} last
+   * @return {void}
+   */
+  setLastName(last) {
+    this.lastName = last;
+  }
+
+  /**
+   * setFullName - set full name (first and last)
+   * @param {String} firstAndLast
+   * @return {void}
+   */
+  setFullName(firstAndLast) {
+    const names = firstAndLast.split(' ');
+    this.setFirstName(names[0]);
+    this.setLastName(names[1]);
+  }
+}
